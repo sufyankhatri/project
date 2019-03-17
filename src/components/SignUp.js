3edd24a01f324f363c2f6c4c0da3660b10d30497
sufyankhatri <43412206+sufyankhatri@users.noreply.github.com>
2019-03-18 00:47:03 +0500

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -12,10 +12,11 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
-import { Link  } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 //import Link from '@material-ui/core/Link';
-import {signUser} from '../actions/AuthActions'
-import {connect} from 'react-redux'
+import { signUser } from '../actions/AuthActions'
+import { connect } from 'react-redux'
+import NativeSelect from '@material-ui/core/NativeSelect';
 const styles = theme => ({
   main: {
     width: 'auto',
@@ -46,76 +47,246 @@ const styles = theme => ({
   submit: {
     marginTop: theme.spacing.unit * 3,
   },
+  error:{
+    color: 'red'
+  }
 });
 
 class SignUp extends React.Component {
-  state={
-    email:'',
-    password:''
+  state = {
+    firstName: '',
+    lastName: '',
+    dob: '',
+    email: '',
+    password: '',
+    cpassword: '',
+    gpa: '',
+    insti: "",
+    Ferror:false,
+    Lerror:false,
+    Gerror:false,
+    Perror:false,
+    Cerror:false
+
   }
-  onHandleChange=(event)=>{
+  onHandleChange = (event) => {
+
     this.setState({
-      [event.target.id]:event.target.value
+      [event.target.id]: event.target.value
     })
   }
-  onSignPressed=()=>{
-    const{email,password}= this.state
-    this.props.signUser(email,password)
-  }  
-  render(){
-  const { classes } = this.props;
+  onFirstNameChange = (event) => {
+    
+    if (event.target.value.length > 5) {
+      this.setState({
+        Ferror:true
+      })
+    }
+    else {
+      this.setState({
+        Ferror:false
+      })
+    }
+  }
+  onLastNameChange= (event)=>{
+    if (event.target.value.length > 5) {
+      this.setState({
+        Lerror:true
+      })
+    }
+    else {
+      this.setState({
+        Lerror:false
+      })
+    }
+  }
+  onPasswordChange=(event)=>{
+    if(event.target.value.length<6){
+      this.setState({
+        Perror:true
+       
+      })
+    }
+    else{
+      this.setState({
+        Perror:false,
+        password:event.target.value
+      })
+    }
+  }
 
-  return (
-    <main className={classes.main}>
-      <CssBaseline />
-      <Paper className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          {/* <LockOutlinedIcon /> */}
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign Up
+  onConfirmPasswordChange=(event)=>{
+    console.log(event.target.value)
+    console.log("password is",this.state.password)
+    if((event.target.value)!==( this.state.password)){
+      this.setState({
+        Cerror:true
+      })
+    }
+    else{
+      this.setState({
+        Cerror:false
+      })
+    }
+  }
+  
+
+  onGpaChange=(event)=>{
+    if(event.target.value>4){
+      this.setState({
+        Gerror:true
+      })
+  
+    }
+    else{
+      this.setState({
+        Gerror:false
+      })
+    }
+  }
+  onInstituteChange=(event)=>{
+    console.log(event.target.value)
+    this.setState({
+     insti:event.target.value
+    });
+    console.log(this.state.insti)
+    
+  }
+
+
+  onSignPressed = () => {
+    const { email, password } = this.state
+    this.props.signUser(email, password)
+  }
+  
+  showFirstNameError=()=>{
+    const{classes}=this.props
+    if(this.state.Ferror){
+      console.log("hello")
+      return(
+        <InputLabel className={classes.error}>First Name should be less than 6 characters</InputLabel>   
+      )
+      }
+    
+  }
+  showPasswordError=()=>{
+    const{classes}= this.props
+    if(this.state.Perror){
+      return(
+        <InputLabel className={classes.error}>Password must be 6 characters long</InputLabel>
+      )
+    }
+  }
+
+  showLastNameError=()=>{
+    const{classes}=this.props
+    if(this.state.Lerror){
+      console.log("hello")
+      return(
+        <InputLabel className={classes.error}>Last Name should be less than 6 characters</InputLabel>   
+      )
+      }
+    
+  }
+showGpaError=()=>{
+    const{classes}=this.props
+    if(this.state.Gerror){
+      return(
+        <InputLabel  className={classes.error}  >Gpa should be less than 4 </InputLabel>
+      )
+    }
+}
+showConfirmPasswordError(){
+  const{classes}=this.props
+  if(this.state.Cerror){
+    return(
+      <InputLabel className={classes.error} >Passwords are not same</InputLabel>
+    )
+  }
+}
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <main className={classes.main}>
+        <CssBaseline />
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            {/* <LockOutlinedIcon /> */}
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign Up
         </Typography>
-        <form className={classes.form}>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="email">Email Address</InputLabel>
-            <Input id="email" name="email" onChange={this.onHandleChange} autoComplete="email" autoFocus />
-          </FormControl>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <Input name="password" type="password" onChange={this.onHandleChange} id="password" autoComplete="current-password" />
-          </FormControl>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="class">Class</InputLabel>
-            <Input name="class" />
-          </FormControl>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="institute">Institute</InputLabel>
-            <Input name="class" />
-          </FormControl>
-         
-          {/* <FormControlLabel
+          <form className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="firstName">First Name</InputLabel>
+              <Input name="firstName" onChange={this.onFirstNameChange} error={this.state.Ferror}/>
+            </FormControl>
+            {this.showFirstNameError()}
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="last name">Last Name</InputLabel>
+              <Input name="lastName" onChange={this.onLastNameChange} error={this.state.Lerror}/>
+            </FormControl>
+            {this.showLastNameError()}
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Email Address</InputLabel>
+              <Input id="email" name="email" onChange={this.onHandleChange} autoComplete="email" />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input name="password" type="password" onChange={this.onPasswordChange} id="password" autoComplete="current-password" />
+            </FormControl>
+            {this.showPasswordError()}
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Confirm Password</InputLabel>
+              <Input name="password" type="password" onChange={this.onConfirmPasswordChange} id="password" autoComplete="current-password" />
+            </FormControl>
+            {this.showConfirmPasswordError()}
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="gpa">GPA</InputLabel>
+              <Input name="gpa" type="number" onChange={this.onGpaChange}/>
+            </FormControl>
+            {this.showGpaError()}
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="institute">Institute</InputLabel>
+              <Input name="institute" />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth >
+            <NativeSelect
+              native
+              onChange={this.onInstituteChange}
+              value={this.state.insti}
+              name="institute"
+              id="insti"
+            >
+              <option value="ned"> ned</option>
+              <option value="ssuet">ssuet</option>
+              <option value="maju">maju</option>
+            </NativeSelect>
+            </FormControl>
+            {/* <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
             label="Remember me"
           /> */}
-          <Button
-          //  type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={this.onSignPressed}
-          >
-            Submit
+            <Button
+              //  type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={this.onSignPressed}
+            >
+              Submit
           </Button>
-        </form>
-      </Paper>
-    </main>
-  );
-        }
+          </form>
+        </Paper>
+      </main>
+    );
+  }
 }
 
 SignUp.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default connect(null,{signUser})(withStyles(styles)(SignUp))
+export default connect(null, { signUser })(withStyles(styles)(SignUp))
